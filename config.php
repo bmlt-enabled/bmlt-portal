4755a3e53e751d4d34e3157e468e927d2276efbc
pjaudiomv <pjaudiomv@gmail.com>
2018-05-15 22:25:22 -0400

    changed stock config, removed manual

--- config.php
+++ config.php
@@ -4,4 +4,3 @@ static $timezone = 'America/New_York';
 static $service_body_name = 'Carolina Region';
 static $service_body_shortname = 'CRNA';
 static $service_body_website = 'http://crna.org';
-static $bmlt_instructional_manual = 'http://www.crna.org/changes/HowtoUpdateAreaMeetingListings.pdf';
